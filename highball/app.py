060e74452e42007157fedf831873c404163db9ee
HongHyunKi <hk7286@naver.com>
2023-05-31 21:53:27 +0900

    fix: 환경변수 확인 API 제거

--- highball/app.py
+++ highball/app.py
@@ -11,7 +11,6 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-
 #aws key 환경 변수 가져오기
 app.config['S3_BUCKET_NAME'] = 'high-ball'
 app.config['S3_ACCESS_KEY'] = os.environ.get('S3_ACCESS_KEY')
@@ -28,16 +27,6 @@ s3 = boto3.client(
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-#환경 변수 access key 확인
-@app.route('/key')
-def key():
-    S3_ACCESS_KEY = app.config['S3_ACCESS_KEY']
-    S3_SECRET_KEY = app.config['S3_SECRET_KEY']
-    print('엑세스',S3_ACCESS_KEY)
-    print('시크릿',S3_SECRET_KEY)
-    
-    return '터미널확인'
 
 #이미지 업로드
 @app.route('/imgupload', methods=['POST'])
